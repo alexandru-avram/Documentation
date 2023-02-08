bc19043cc97d8605782f691fa4f0842a817cab67
Alexandru Avram-Rusu <83804634+alexandruavram-rusu@users.noreply.github.com>
2023-02-08 10:02:17 +0200

    Update 01.Color_Changer.js

--- HTML/Document Object Model/01.Color_Changer.js
+++ HTML/Document Object Model/01.Color_Changer.js
@@ -20,9 +20,9 @@ header.style.color = 'red'
 // http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
 function getRandomColor(){
   var letters = "0123456789ABCDEF";
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random()*16)];
+  var color = "#";
+  for (var i = 0; i < 6; i++){
+    color += letters[Math.floor.random()*16];
   }
   return color
 }
