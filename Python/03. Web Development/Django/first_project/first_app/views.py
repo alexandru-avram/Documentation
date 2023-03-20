fb92c59530f50aef2a4470751d7465e269549bd8
Alexandru Avram-Rusu <83804634+alexandruavram-rusu@users.noreply.github.com>
2023-03-20 15:57:37 +0200

    templates

--- Python/03. Web Development/Django/first_project/first_app/views.py
+++ Python/03. Web Development/Django/first_project/first_app/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here
 
+# 
+# def index(request):
+#    return HttpResponse("<em>Simple text with HTML tags</em>")
+
 def index(request):
-    return HttpResponse("<em>Hello World!</em>")
+    my_dict = {'insert_me': "Inserting a text using a template tag"}
+    return render(request, 'first_app/index.html', context=my_dict)
