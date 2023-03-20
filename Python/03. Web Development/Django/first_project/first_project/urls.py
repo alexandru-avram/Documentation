53f893e70eb8841a94cde9db445a899ab4f65f6b
Alexandru Avram-Rusu <83804634+alexandruavram-rusu@users.noreply.github.com>
2023-03-20 13:18:48 +0200

    urlpatterns

--- Python/03. Web Development/Django/first_project/first_project/urls.py
+++ Python/03. Web Development/Django/first_project/first_project/urls.py
@@ -19,7 +19,8 @@ from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first_app/', include('first_app.urls'))
+    path('first_app/', include('first_app.urls')),
+    path('', views.index, name='index')
     
     
 ]
