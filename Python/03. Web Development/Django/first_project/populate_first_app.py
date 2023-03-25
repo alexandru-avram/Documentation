4a0e5605d9f00d32ab96191551bc854fe22f799b
Alexandru Avram-Rusu <alexandru.avramrusu@gmail.com>
2023-03-25 19:04:24 +0200

    population script

--- Python/03. Web Development/Django/first_project/populate_first_app.py
+++ Python/03. Web Development/Django/first_project/populate_first_app.py
@@ -1,18 +1,19 @@
 import os
-# Configure settings for project
-# Need to run this before calling models from application!
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','first_project.settings')
-
-from django.conf import settings
-# Import settings
-settings.configure()
-
+import sys
+import django
 import random
-from first_app.models import Topic,Webpage,AccessRecord
 from faker import Faker
 
-fakegen = Faker()
-topics = ['Search','Social','Marketplace','News','Games']
+# Importing first_project
+project_path = os.path.abspath(os.path.join(os.getcwd(), '../'))
+sys.path.append(project_path)
+from first_app.models import AccessRecord, Webpage, Topic
+
+
+# Setting up and configuring the porject settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.first_project.settings')
+django.setup()
+
 
 ### Fake population script
 
@@ -32,28 +33,22 @@ topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
  
 
 def populate(N=5):
-    '''
-    Create N Entries of Dates Accessed
-    '''
-
     for entry in range(N):
-
-        # Get Topic for Entry
+        
+        # get the topic for the entry
         top = add_topic()
-
-        # Create Fake Data for entry
+        
+        # create the fake date for that entry
         fake_url = fakegen.url()
         fake_date = fakegen.date()
         fake_name = fakegen.company()
-
-        # Create new Webpage Entry
-        webpg = Webpage.objects.get_or_create(topic=top,url=fake_url,name=fake_name)[0]
-
-        # Create Fake Access Record for that page
-        # Could add more of these if you wanted...
-        accRec = AccessRecord.objects.get_or_create(name=webpg,date=fake_date)[0]
-
-
+        
+        # create the new webapge entry
+        webpg = Webpage.objects.get_or_create(topic = top, url = fake_url, name = fake_name)[0]
+        
+        # create a fake access record for that webpage
+        acc_rec = AccessRecord.objects.get_or_create(name = webpg, date=fake_date)[0]
+        
 if __name__ == '__main__':
     print("populating script!")
     populate(20)
