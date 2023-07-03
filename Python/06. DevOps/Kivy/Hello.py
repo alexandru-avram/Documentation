1f96d52f2508c92e3974277967d46ef02a277a90
Alexandru Avram-Rusu <alexandru.avramrusu@gmail.com>
2023-07-03 21:21:02 +0300

    Update Hello.py

--- Python/06. DevOps/Kivy/Hello.py
+++ Python/06. DevOps/Kivy/Hello.py
@@ -1,2 +1,12 @@
 import kivy
 from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def buid(self):
+        return Label (text="Hello World")
+    
+
+if __name__== '__main__':
+    MyApp().run()
