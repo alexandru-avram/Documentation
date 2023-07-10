1b2e7e716fdeae68bd02131fdebfe5b7acfedf8a
Alexandru Avram-Rusu <alexandru.avramrusu@gmail.com>
2023-07-10 21:35:37 +0300

    inheritance

---
+++ Python/06. DevOps/Kivy/04. Widget properties inheritance/04. Inherit.py
@@ -0,0 +1,22 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+# VS Code will not recognize it, but it will run
+from kivy.properties import ObjectProperty
+
+# Import deisgn file
+Builder.load_file("inherit.kv")
+
+class MyLayout(Widget):
+    pass
+
+
+class AwsomeApp(App): 
+    def build(self):
+        return MyLayout()
+
+
+if __name__== '__main__':
+    AwsomeApp().run()
