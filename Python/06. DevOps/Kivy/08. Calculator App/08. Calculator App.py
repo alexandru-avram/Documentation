0bfc36d306df8ca5f1c2fa856d08fd93dd789784
Alexandru Avram-Rusu <alexandru.avramrusu@gmail.com>
2023-07-26 20:42:43 +0300

    calculator app

--- Python/06. DevOps/Kivy/08. Calculator App/08. Calculator App.py
+++ Python/06. DevOps/Kivy/08. Calculator App/08. Calculator App.py
@@ -1,4 +1,5 @@
 import kivy
+import re
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
@@ -21,6 +22,10 @@ class MyLayout(Widget):
         # Create a variable that contains whatever was in the text input
         prior = self.ids.calc_input.text 
 
+        # Test for error first
+        if "Error" in prior:
+            prior = ""
+
         # Determine if 0 is sitting there
         if prior == "0":
             self.ids.calc_input.text = ""
@@ -28,41 +33,79 @@ class MyLayout(Widget):
         else:
             self.ids.calc_input.text = f"{prior}{button}"
 
-    # Create addition function
-    def add(self):
+    # Create a function to 
+    def math_sign(self, sign):
         prior = self.ids.calc_input.text 
-        # Slap a + sign to the text
-        self.ids.calc_input.text =f"{prior}+"
 
-    # Create subtract function
-    def subtract(self):
-        prior = self.ids.calc_input.text 
-        self.ids.calc_input.text =f"{prior}-"
+        # 
+        self.ids.calc_input.text =f"{prior}{sign}"
 
-    # Create multiply function
-    def multiply(self):
-        prior = self.ids.calc_input.text 
-        self.ids.calc_input.text =f"{prior}*"
 
-    # Create divide function
-    def divide(self):
-        prior = self.ids.calc_input.text 
-        self.ids.calc_input.text =f"{prior}/"
+    # Create a function to backspace, remove last character
+    def remove(self):
+        prior = self.ids.calc_input.text
+
+        # Remove the last item in the textbox
+        prior = prior[:-1]
+
+        if prior == "":
+            prior = "0"
+        else:
+            pass
+
+        self.ids.calc_input.text = prior
+
+    # Create function to make input positive or negative:
+    def pos_neg(self):
+        prior = self.ids.calc_input.text
+
+        # Test to see if there is a negative sign
+        if "-" in prior:
+            self.ids.calc_input.text = f"{prior.replace('-','')}"
+        else:
+            self.ids.calc_input.text = f"-{prior}"
+
+    # Create a decimal function
+    def dot(self):
+        prior = self.ids.calc_input.text
+
+        # Split our text box by math sign
+        num_list = re.split(r"[+\-*/]", prior)
+        
+        if "+" in prior and "." not in num_list[-1]:
+            prior = f"{prior}."
+            self.ids.calc_input.text = prior
+        
+        elif "." in prior:
+            pass
+
+        else:
+            # Add decimal to the end of the input
+            prior = f"{prior}."
+
+            # Output back to the input
+            self.ids.calc_input.text = prior
 
     def equals(self):
         prior = self.ids.calc_input.text 
 
-        # Addition
-        if "+" in prior:
-            num_list = prior.split("+")
-            answer = 0
-            # loop through out list
-            for number in num_list:
-                answer = answer + int(number)
+        # Error handling
+        try:
+            # Evaluate the math from the text box
+            answer = eval(prior)
 
-            # print the answer in the text box
+            # Output the answer
+            answer = str(answer)
+
+            if answer[-2:] == ".0":
+                answer = answer[:-2]
+            else:
+                pass
+
             self.ids.calc_input.text = str(answer)
-    
+        except:
+            self.ids.calc_input.text = "Error"
+
 
 class  CalculatorApp(App): 
     def build(self):
@@ -70,4 +113,4 @@ class  CalculatorApp(App):
 
 
 if __name__== '__main__':
-    CalculatorApp().run()
+    CalculatorApp().run() 
