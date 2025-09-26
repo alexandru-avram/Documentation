54441d8af87615cc91c46186fab1702a658f6ea2
Alexandru Avram-Rusu <alexandru.avramrusu@gmail.com>
2025-09-26 12:08:11 +0300

    Create Day1 - Band Name Generator.py

---
+++ Python/100 Days of Code Bootcamp/Day1 - Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city your grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("Your band should be called " + city_name + " " + pet_name)
