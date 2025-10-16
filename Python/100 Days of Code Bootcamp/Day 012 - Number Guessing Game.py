55de567f95bb8badf7bf2ec474a147178f0134f4
Alexandru Avram-Rusu <alexandru.avramrusu@gmail.com>
2025-10-16 13:08:19 +0300

    Update Day 012 - Number Guessing Game.py

--- Python/100 Days of Code Bootcamp/Day 012 - Number Guessing Game.py
+++ Python/100 Days of Code Bootcamp/Day 012 - Number Guessing Game.py
@@ -13,7 +13,9 @@ def clear_logo():
           """)
     print("Welcome to the Number Guessing Game!")
     print("\n\n")
+    print("I'm thinking of a number between 1 and 100. Can you guess it?")
 
+# Check if my guess is equal to the random number
 def number_check():
     global number_to_guess
     global my_guess
@@ -52,18 +54,28 @@ number_to_guess = random.randint(0,100)
 my_guess = 101
 attempts = 0
 
-# INTRO HERE
+# START HERE
+# INTRO
 clear_logo()
-print("I'm thinking of a number between 1 and 100. Can you guess it?")
 
+# CHOOSE A DIFFICULTY
 chose_difficulty()
 
-
+# GAME LOOP
 while my_guess != number_to_guess and attempts > 0:
     print(f"You have {attempts} attempts remaining to guess the number.")
-    my_guess = int(input("Make a guess: "))
-
+    
+    # Modified to create an exception handling example
+    # While True is necessary to keep a loop on the try/except
+    while True:
+        try:
+            my_guess = int(input("Make a guess: "))
+            break
+        except ValueError:
+            attempts -= 1
+            print(f"That was not a number. You lost an attemp. You have {attempts} remaining.")
+            
     number_check()
 
-
+# END
 input("Thank you for playing. Press 'ENTER' to continue...")
